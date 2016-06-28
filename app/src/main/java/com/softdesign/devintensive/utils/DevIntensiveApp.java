ec3c4cd405e78d089a4ba1250934eeae633cec26
Nick <nick-dev@bk.ru>
2016-06-28 22:52:39 +0400

    Added DevIntensiveApp class

---
+++ app/src/main/java/com/softdesign/devintensive/utils/DevIntensiveApp.java
@@ -0,0 +1,23 @@
+package com.softdesign.devintensive.utils;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by nick on 28.06.16.
+ */
+public class DevIntensiveApp extends Application {
+    private static SharedPreferences sSharedPreferences;
+
+    public SharedPreferences getSharedPreferences()
+    {
+        return  sSharedPreferences;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
+}
